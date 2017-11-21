4aa59e0b9a308310ab572bc97bbbddc6968a4c3b
679918 <679918@ELF6IT215951.ACDM.DS.SAIT.CA>
2017-11-21 08:19:18 -0700

    Week10 Lab completed - added the required admin filter

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -19,7 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 public class AdminFilter implements Filter {
     
         private FilterConfig filterConfig = null;
